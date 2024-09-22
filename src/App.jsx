0a313fc34ce56ea4114b4df95c30cd50912e78ef
codebylalit <lalitkumarnamdev4645@gmail.com>
2024-09-22 11:53:18 +0530

    ds

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
-          {/* <Route path='/' element={<Home />} /> */}
+          <Route path='/' element={<About />} />
           <Route
             path='/*'
             element={
               <>
                 <Routes>
-                  <Route path='/' element={<About />} />
+                  <Route path='/about' element={<About />} />
                   <Route path='/projects' element={<Projects />} />
                   <Route path='/contact' element={<Contact />} />
                 </Routes>
